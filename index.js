e89da6e9f6bbbca227c1ef1ce34eedf5e9584ddc
MaxymMalinin <maxmalinin@gmail.com>
2024-10-09 12:36:18 +0300

    pay 3000 - 2024-09-06 + pay 3000 - 2024-10-04

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // const moneyVeoBorg = 4727.49; // old
 // const moneyVeoBorg = 3846.82; // old 2 // total for 2024-07-05 -3000.00 (pay) - 70.91 (fee)
+// const moneyVeoBorg = 8477.46; // total for 2024-08-06 - 70.91 (fee)
+// const moneyVeoBorg = 18226.6; // total for 2024-09-06 - 70.91 (fee)
 
-const moneyVeoBorg = 8477.46; // total for 2024-08-06 - 70.91 (fee)
+const moneyVeoBorg = 36389.32; // total for 2024-10-04 - 70.91 (fee)
 const moneyVeoFee = 70.91;
 const moneyVeoPercent = 2.5;
 
 // const limonBorg = 5016; //old
 // const limonBorg = 6271.17; // old 2 // total for 2024-07-05 - 50.16 (fee)
+// const limonBorg = 6648.62; // total for 2024-08-06 - 3450 (pay) - 50.16 (fee)
+// const limonBorg = 7548.22; // total for 2024-09-06 - 3000 (pay) - 50.16 (fee)
 
-const limonBorg = 6648.62; // total for 2024-08-06 - 3450 (pay) - 50.16 (fee)
+const limonBorg = 8452.33; // total for 2024-09-06 - 3000 (pay) - 50.16 (fee)
 const limonFee = 50.16;
 const limonPercent = 1.5;
 
 // const payDate = new Date('2024-06-20'); // old
 // const payDate = new Date('2024-07-05'); // old 2
 
-const payDate = new Date('2024-08-06');
+const payDate = new Date('2024-10-04');
 
 const countCurrentBorg = (borg, fee, percent, startDate) => {
   const millisecondsInOneDay = 24 * 60 * 60 * 1000;
